50f46ae36d4c4eb68bc14a8f84c342951003980f
Eamon Conway <conway.e@wehi.edu.au>
2023-03-29 14:41:07 +1100

    Define basic types.
    To help with the delcaration of different parameters, I am implementing a baseline types header.
    This should help at a later date if there are any complicated inclusions.

---
+++ include/PlasX/types.hpp
@@ -0,0 +1,16 @@
+#ifndef PLASX_TYPES_HPP
+#define PLASX_TYPES_HPP
+/**
+ * @file types.hpp
+ * @author Eamon Conway (conway.e@wehi.edu.au)
+ * @brief 
+ * @version 0.1
+ * @date 2023-03-29
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+namespace plasx {
+  using RealType = double;
+}
+#endif
