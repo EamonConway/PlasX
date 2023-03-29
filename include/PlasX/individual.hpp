6ce6fa843ba3eff89632bdfe04b92cd89c456310
Eamon Conway <conway.e@wehi.edu.au>
2023-03-29 14:39:04 +1100

    Define individual type for transmission simulation.
    The individual class will contain all individual specific information used within a simulation.
    The current implementation treats DiseaseStatus as a tempalte parameter for easily updating diseases.
    Currently there are no individual specific functions that are of use, but inclusions could be made in the future.

---
+++ include/PlasX/individual.hpp
@@ -0,0 +1,44 @@
+#ifndef PLASX_INDIVIDUAL_HPP
+#define PLASX_INDIVIDUAL_HPP
+/**
+ * @file individual.hpp
+ * @author Eamon Conway (conway.e@wehi.edu.au)
+ * @brief
+ * @version 0.1
+ * @date 2023-03-29
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+#include "PlasX/types.hpp"
+namespace plasx {
+/**
+ * @details Individuals depend upon their underlying disease status.
+ * We use their disease status as a template parameter so that we can
+ * efficiently change the type of individual that we run the simulation for.
+ * @tparam DiseaseStatus is a class that stores all individual specific
+ * information for the disease.
+ */
+template <typename DiseaseStatus>
+/**
+ * @details Individual is a class that contains all individual specific
+ * information.
+ *
+ */
+class Individual {
+ public:
+  /**
+   * @brief Construct a new Individual object
+   *
+   * @param age
+   * @param dargs Parameter pack that is forwarded to the constructor of the
+   * underlying status_.
+   */
+  Individual(double age, auto&&... dargs)
+      : age_(age), status_(std::forward<decltype(dargs)>(dargs)...){};
+
+  RealType age_;
+  DiseaseStatus status_;
+};
+}  // namespace plasx
+#endif
