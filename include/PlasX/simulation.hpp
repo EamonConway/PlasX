b9b9101226abcd40f66856b751d716845a5d3c63
Eamon Conway <conway.e@wehi.edu.au>
2023-04-18 13:43:13 +1000

    Define generalised function for a simulation.
    A simulation, irrespective of what strain, should be able to follow a loop of while time is less than end time, run this function.
    Therefore I have defined a templated simulation function.
    The ability of this function to fully encapsulate everything we need is not yet fully established.
    This will be established through the course of development.

---
+++ include/PlasX/simulation.hpp
@@ -0,0 +1,32 @@
+#ifndef PLASX_SIMULATION_HPP
+#define PLASX_SIMULATION_HPP
+#include <algorithm>
+
+#include "PlasX/types.hpp"
+
+namespace plasx {
+
+/**
+ * @brief
+ *
+ * @tparam OneStepFunction
+ * @tparam OneStepArgs
+ * @param t0
+ * @param t1
+ * @param dt
+ * @param one_step
+ * @param function_args
+ * @return RealType
+ */
+template <class OneStepFunction, class... OneStepArgs>
+RealType simulation(const double t0, const double t1, const double dt,
+                    OneStepFunction one_step, OneStepArgs... function_args) {
+  auto t = t0;
+
+  while (t < t1) {
+    auto t = one_step(t, dt, function_args);
+  }
+  return t;
+}
+}  // namespace plasx
+#endif
