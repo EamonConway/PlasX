65e09d88316b8df384d1d12b4f21013f2d7e2711
Eamon Conway <conway.e@wehi.edu.au>
2023-04-18 11:43:41 +1000

    Source code for PFalc class.
    The PFalc object can be initialised using an arbitrary disease state (compartment) and any immunity level.
    
    Member function getIC and getIA allow for the return of the current immunity level.
    
    The functions clearInfectionQueue, scheduleInfection and updateInfection control the queueing of future infections for the PFalc object.
    This means we do not have to store any past data for the lagged force of infection.
    
    We have provided functionality for the caching of the next infection within the PFalc object.
    This has been done to minimise the indirection that is caused when searching for the infection_queue_.
    It is expected that this will minimise the number of cache misses and therefore increase the efficiency of the simulation.

---
+++ src/PlasX/Falciparum/griffin.cpp
@@ -0,0 +1,62 @@
+#include "PlasX/Falciparum/griffin.hpp"
+
+#include "PlasX/random.hpp"
+
+namespace plasx {
+namespace falciparum {
+namespace griffin {
+// Construct the object that will store the information in the Griffin
+// simulation.
+PFalc::PFalc(const Status& status, double ICA, double ICM, double IA)
+    : current_(status),
+      I_CA_(ICA),
+      I_CM_(ICM),
+      I_A_(IA),
+      cached_infection_(std::numeric_limits<double>::infinity()){};
+
+double PFalc::getIC() { return I_CA_ + I_CM_; }
+
+double PFalc::getIA() { return I_A_; }
+
+void PFalc::clearInfectionQueue() {
+  // Replace with an empty queue.
+  infection_queue_ =
+      std::priority_queue<double, std::vector<double>, std::less<double>>();
+}
+
+void PFalc::scheduleInfection(const double t) {
+  const auto isCacheable = t < cached_infection_;
+  // You have to do the push first. or you might overright the cached value and
+  // lose it
+  infection_queue_.push(!isCacheable * t + isCacheable * cached_infection_);
+  cached_infection_ = isCacheable * t + !isCacheable * cached_infection_;
+  // Arguably faster than having the if statements.... will have to check.
+};
+
+bool PFalc::updateInfection(const double t) {
+  // Technically, by using a priority queue there is an indirection to the
+  // stack as all data is held in a vector. We could consider caching the top
+  // value in the individual structure and only go away once that value is
+  // outdated. If the infectionQueue is empty, the next infection could be at
+  // time infinity. We would have to make sure that we update things
+  // appropriately for that however. This may not be the best, at a new
+  // infection we would have to check against cache value. If cahced value is
+  // larger, we need to move it bak into the queue. But this might just be fine.
+  // It is an interesting thing to look for.
+  if (t >= cached_infection_) [[unlikely]] {
+    // The cached infection is going to take place.
+    if (infection_queue_.empty()) {
+      cached_infection_ = std::numeric_limits<double>::infinity();
+    } else {
+      cached_infection_ = infection_queue_.top();
+      infection_queue_.pop();
+    }
+    return true;
+  } else [[likely]] {
+    return false;
+  }
+}
+
+}  // namespace griffin
+}  // namespace falciparum
+}  // namespace plasx
