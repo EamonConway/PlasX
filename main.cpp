c4ea19567e1bfe88b19134335f30c968fd4ca980
Eamon Conway <conway.e@wehi.edu.au>
2023-06-14 17:23:47 +1000

    Run the switch function in main

--- main.cpp
+++ main.cpp
@@ -18,24 +18,10 @@ int main() {
     population.emplace_back(10.0, pfg::Status::S, 0.0, 0.0, 0.0);
   }
 
-  // Run a simulation - as this function is templated, it should be possible to
-  // add arbitrary numbers of inputs. This will be helpful for establishing
-  // any interventions.
-  std::vector<size_t> S, A, U, D, T, P, Dead;
-  for (int i = 0; i < population.size(); ++i) {
-    S.emplace_back(i);
-  }
-  A.reserve(N);
-  U.reserve(N);
-  D.reserve(N);
-  T.reserve(N);
-  P.reserve(N);
-
-
   auto start = std::chrono::steady_clock::now();
   [[maybe_unused]] auto t2 =
-      plasx::simulation(0.0_yrs, 1.0_yrs, 1.0_days, pfg::one_step_no_switch,
-                        population, params, 1.0,S, A, U, D, T, P, Dead);
+      plasx::simulation(0.0_yrs, 1.0_yrs, 1.0_days, pfg::one_step_switch,
+                        population, params, 1.0);
   auto end = std::chrono::steady_clock::now();
   auto elapsed_seconds = end - start;
   std::cout << "elapsed time with no switches: " << elapsed_seconds.count() * 1e-6 << "\n";
