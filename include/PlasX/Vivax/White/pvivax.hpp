751bb399e9dfab71385195d208e44cb92088fd64
Eamon Conway <conway.e@wehi.edu.au>
2023-07-17 16:26:31 +1000

    Define the state of Pvivax infections.
    To simulate Pvivax withint the plasx framework, we must define the class that will store neccesary information about the individual.
    Currently includes everything required for a simple equilibrium simulation.
    In the future this should be updated with extra componenets.

---
+++ include/PlasX/Vivax/White/pvivax.hpp
@@ -0,0 +1,145 @@
+/**
+ * @file pvivax.hpp
+ * @author Eamon Conway (conway.e@wehi.edu.au)
+ * @brief
+ * @version 0.1
+ * @date 2023-06-16
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+#ifndef PLASX_VIVAX_WHITE_PVIVAX_HPP
+#define PLASX_VIVAX_WHITE_PVIVAX_HPP
+#include <iostream>
+
+#include "PlasX/Vivax/White/infection_scheduler.hpp"
+#include "PlasX/Vivax/White/parameters.hpp"
+#include "PlasX/Vivax/White/status.hpp"
+#include "PlasX/individual.hpp"
+
+namespace plasx {
+namespace vivax {
+namespace white {
+/**
+ * @brief Class containing all individual specific information for the White
+ * model.
+ *
+ */
+class PVivax {
+ public:
+  /**
+   * @brief Construct a new PVivax object
+   *
+   * @param status
+   * @param parasite_immunity
+   * @param clinical_immunity
+   * @param maternal_parasite_immunity
+   * @param maternal_clinical_immunity
+   * @param zeta
+   */
+  PVivax(const RealType age, const Status& status,
+         const RealType parasite_immunity, const RealType clinical_immunity,
+         const RealType maternal_parasite_immunity,
+         const RealType maternal_clinical_immunity, const RealType zeta,
+         const RealType rho, const RealType age_0,
+         const SizeType n_hypnozoites);
+
+  /**
+   * @brief Construct a new PVivax object - should be used for the birth of an
+   * individual.
+   *
+   * @param status
+   * @param maternal_parasite_immunity
+   * @param maternal_clinical_immunity
+   * @param zeta
+   */
+  PVivax(const RealType age, const Status& status,
+         const RealType maternal_parasite_immunity,
+         const RealType maternal_clinical_immunity, const RealType zeta,
+         const RealType rho, const RealType age_0);
+
+  /**
+   * @brief What happens when an infection is triggered.
+   *
+   */
+  void queueInfection(const RealType t, const RealType lambda,
+                      const RealType total_prob);
+
+  /**
+   * @brief Check if an infection event has to occur.
+   *
+   * @param t
+   * @param u_par
+   * @return true
+   * @return false
+   */
+  bool updateInfection(const RealType t, const RealType u_par);
+
+  SizeType& getNumHypnozoites() noexcept { return num_hypnozoites_; }
+
+  /**
+   * @brief Clear the queue of infections that will occur. This is only called
+   * when an individual will go into the treated compartment.
+   *
+   */
+  void clearInfectionQueue() noexcept;
+
+  /**
+   * @brief
+   *
+   * @param t
+   * @param refractory_period
+   */
+  void trackImmunityBoosts(const RealType t,
+                           const RealType refractory_period) noexcept;
+
+  void update(const RealType dt);
+
+  void updateImmunity(const RealType dt,
+                      const RealType exp_rate_dt_parasite_immunity,
+                      const RealType exp_rate_dt_clinical_immunity,
+                      const RealType exp_rate_dt_maternal_immunity,
+                      const RealType age,
+                      const RealType end_maternal_immunity) noexcept;
+
+  /**
+   * @brief Get the heterogeneity in biting for an individual.
+   *
+   * @return RealType
+   */
+  const RealType& getZeta() const noexcept { return zeta_; };
+
+  const RealType& getOmega() const noexcept { return omega_; };
+  void setOmega(const RealType& omega) noexcept { omega_ = omega; };
+
+  // get functions call the old levels of immunity as the others have been
+  // updated slightly in the switch
+  const RealType getParasiteImmunity() const noexcept {
+    return parasite_immunity_ + maternal_parasite_immunity_;
+  };
+  const RealType getClinicalImmunity() const noexcept {
+    return clinical_immunity_ + maternal_clinical_immunity_;
+  };
+
+  /**
+   * @brief Current state of the individual.
+   *
+   */
+  Status current_;
+
+ private:
+  RealType parasite_immunity_, boosts_parasite_immunity_;
+  RealType clinical_immunity_, boosts_clinical_immunity_;
+  RealType maternal_parasite_immunity_;
+  RealType maternal_clinical_immunity_;
+  RealType time_refractory_period_over_;
+  RealType zeta_;
+  RealType omega_;
+  SizeType num_hypnozoites_;
+  InfectionScheduler infection_queue_;
+  friend std::ostream& operator<<(std::ostream& os, const PVivax& data);
+};
+}  // namespace white
+}  // namespace vivax
+}  // namespace plasx
+#endif
---
+++ src/PlasX/Vivax/White/pvivax.cpp
@@ -0,0 +1,127 @@
+#include "PlasX/Vivax/White/pvivax.hpp"
+
+#include <cmath>
+#include <iostream>
+
+#include "PlasX/random.hpp"
+#include "PlasX/udl.hpp"
+namespace plasx {
+namespace vivax {
+namespace white {
+
+std::ostream& operator<<(std::ostream& os, const PVivax& data) {
+  os << data.zeta_ << ", ";
+  os << data.parasite_immunity_ << ", ";
+  os << data.clinical_immunity_ << ", ";
+  os << data.maternal_parasite_immunity_ << ", ";
+  os << data.maternal_clinical_immunity_ << ", ";
+  os << data.num_hypnozoites_ << ", ";
+  os << data.current_;
+  return os;
+}
+
+const auto delay = 10.0;  // Become part of parameters?
+
+// Birth Constructor.
+PVivax::PVivax(const RealType age, const Status& status,
+               const RealType maternal_parasite_immunity,
+               const RealType maternal_clinical_immunity, const RealType zeta,
+               const RealType rho, const RealType age_0)
+    : PVivax(age, status, 0.0, 0.0, maternal_parasite_immunity,
+             maternal_clinical_immunity, zeta, rho, age_0, 0){};
+
+// Typical Constructor
+PVivax::PVivax(const RealType age, const Status& status,
+               const RealType parasite_immunity,
+               const RealType clinical_immunity,
+               const RealType maternal_parasite_immunity,
+               const RealType maternal_clinical_immunity, const RealType zeta,
+               const RealType rho, const RealType age_0,
+               const SizeType n_hypnozoites)
+    : current_(status),
+      parasite_immunity_(parasite_immunity),
+      boosts_parasite_immunity_(0.0),
+      clinical_immunity_(clinical_immunity),
+      boosts_clinical_immunity_(0.0),
+      maternal_parasite_immunity_(maternal_parasite_immunity),
+      maternal_clinical_immunity_(maternal_clinical_immunity),
+      time_refractory_period_over_(std::numeric_limits<RealType>::lowest()),
+      zeta_(zeta),
+      omega_(1.0 - rho * std::exp(-age / age_0)),
+      num_hypnozoites_(n_hypnozoites){};
+
+void PVivax::queueInfection(const RealType t, const RealType lambda,
+                            const RealType total_prob) {
+  auto num_new_hypnozoites = 0;
+  auto bitten = genunf_std(generator) < lambda / total_prob;
+  if (bitten) {
+    // Track the number of hypnozoites that are made for this infection. Will be
+    // added to the PVivax class when the infection is triggered.
+    num_new_hypnozoites += 1;
+  }
+  infection_queue_.emplace(t + delay, num_new_hypnozoites);
+};
+
+bool PVivax::updateInfection(const RealType t, const RealType u_par) {
+  if (infection_queue_.empty()) {
+    return false;
+  }
+
+  auto activate_infection = t >= infection_queue_.top().getInfectionTime();
+  if (!activate_infection) {
+    return false;
+  }
+
+  while (!infection_queue_.empty() &&
+         t >= infection_queue_.top().getInfectionTime()) {
+    // For each infection that activates, we want to update the immunity and add
+    // the appropriate number of hypnozoites.
+    num_hypnozoites_ += infection_queue_.top().getNumHypnozoites();
+    trackImmunityBoosts(t, u_par);
+    infection_queue_.pop();
+  }
+  return true;
+}
+
+void PVivax::trackImmunityBoosts(const RealType t,
+                                 const RealType refractory_period) noexcept {
+  auto boosting_can_occur = t >= time_refractory_period_over_;
+  if (boosting_can_occur) {
+    ++boosts_parasite_immunity_;
+    ++boosts_clinical_immunity_;
+    time_refractory_period_over_ = t + refractory_period;
+  }
+}
+
+void PVivax::updateImmunity(const RealType dt,
+                            const RealType exp_rate_dt_parasite_immunity,
+                            const RealType exp_rate_dt_clinical_immunity,
+                            const RealType exp_rate_dt_maternal_immunity,
+                            const RealType age,
+                            const RealType end_maternal_immunity) noexcept {
+  // Apply the decay rate onto immunity.
+  parasite_immunity_ *= exp_rate_dt_parasite_immunity;
+  parasite_immunity_ += boosts_parasite_immunity_;
+  clinical_immunity_ *= exp_rate_dt_clinical_immunity;
+  clinical_immunity_ += boosts_clinical_immunity_;
+  maternal_parasite_immunity_ *= exp_rate_dt_maternal_immunity;
+
+  auto zero_maternal_immunity = age >= end_maternal_immunity;
+  if (zero_maternal_immunity) {
+    maternal_parasite_immunity_ = 0.0;
+    maternal_clinical_immunity_ = 0.0;
+  }
+
+  boosts_parasite_immunity_ = 0.0;
+  boosts_clinical_immunity_ = 0.0;
+}
+
+void PVivax::clearInfectionQueue() noexcept {
+  // It is faster to clear elements than replace with an empty queue.
+  while (infection_queue_.size() != 0) {
+    infection_queue_.pop();
+  }
+}
+}  // namespace white
+}  // namespace vivax
+}  // namespace plasx
