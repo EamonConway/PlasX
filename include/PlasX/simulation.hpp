3647735f8febae4e96bf03970e2b0762c08f09c5
Eamon Conway <conway.e@wehi.edu.au>
2023-07-17 15:22:23 +1000

    Allow plasx::simulation to choose appropriate function in the case of overloaded definitions.
    The api that I have decided upon uses a templated simulation function to run the one_step code.
    However, if the one_step function is overloaded, then automatic template type deduction fails.
    The OneSteFunctionSelector will make sure that the appropriate overloaded function is called at compile time.

--- include/PlasX/simulation.hpp
+++ include/PlasX/simulation.hpp
@@ -1,31 +1,40 @@
 #ifndef PLASX_SIMULATION_HPP
 #define PLASX_SIMULATION_HPP
 #include <algorithm>
+#include <concepts>
+#include <iostream>
+#include <tuple>
+#include <type_traits>
+#include <utility>
 
 #include "PlasX/types.hpp"
-
 namespace plasx {
-
-/**
- * @brief
- *
- * @tparam OneStepFunction
- * @tparam OneStepArgs
- * @param t0
- * @param t1
- * @param dt
- * @param one_step
- * @param function_args
- * @return RealType
- */
+// Function object to select the correct OneStepFunction overload
 template <class OneStepFunction, class... OneStepArgs>
-RealType simulation(const double t0, const double t1, const double dt,
-                    OneStepFunction one_step, OneStepArgs&&... function_args) {
-  auto t = t0;
+struct OneStepFunctionSelector {
+  static auto call(OneStepFunction&& func, OneStepArgs&&... args) {
+    // Check if invoking func with the provided arguments is valid
+    static_assert(
+        std::invocable<OneStepFunction, OneStepArgs...>,
+        "Type OneStepFunction is not invocable with the arguments provided.");
 
+    // Call the appropriate overload using the OneStepFunctionSelector
+    return std::invoke(std::forward<OneStepFunction>(func),
+                       std::forward<OneStepArgs>(args)...);
+  }
+};
+
+// The simulation function
+template <class OneStepFunction, class... OneStepArgs>
+RealType simulation(const RealType t0, const RealType t1, const RealType dt,
+                    OneStepFunction one_step, OneStepArgs&&... function_args) {
+  // Call the appropriate overload using the OneStepFunctionSelector
+  auto t = t0;
   while (t < t1) {
-    t = one_step(t, dt,
-                 std::forward<decltype(function_args)>(function_args)...);
+    t = OneStepFunctionSelector<
+        OneStepFunction, const RealType&, const RealType&,
+        OneStepArgs...>::call(std::forward<OneStepFunction>(one_step), t, dt,
+                              std::forward<OneStepArgs>(function_args)...);
   }
   return t;
 }
