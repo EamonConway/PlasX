b5484eb87b72779b689b074821ae4941aed08b97
Eamon Conway <conway.e@wehi.edu.au>
2023-07-18 17:52:47 +1000

    The plasx::simulation function was not allowing overloaded one_step.
        I wanted the functionality to overload one_step so that I could use an arbitrary eir function.
        This was not possible with the implementation that I had used.
        Also the previous implementation added very little to the code base.
        I have gone back to the old simulation function but have included a check to make sure that the OneStepFunction type is invocable with the appropriate arguments.
        This should minimise any horrible error types that will arise at compile time.

--- include/PlasX/simulation.hpp
+++ include/PlasX/simulation.hpp
@@ -1,40 +1,19 @@
 #ifndef PLASX_SIMULATION_HPP
 #define PLASX_SIMULATION_HPP
-#include <algorithm>
 #include <concepts>
-#include <iostream>
-#include <tuple>
-#include <type_traits>
-#include <utility>
 
 #include "PlasX/types.hpp"
 namespace plasx {
-// Function object to select the correct OneStepFunction overload
-template <class OneStepFunction, class... OneStepArgs>
-struct OneStepFunctionSelector {
-  static auto call(OneStepFunction&& func, OneStepArgs&&... args) {
-    // Check if invoking func with the provided arguments is valid
-    static_assert(
-        std::invocable<OneStepFunction, OneStepArgs...>,
-        "Type OneStepFunction is not invocable with the arguments provided.");
-
-    // Call the appropriate overload using the OneStepFunctionSelector
-    return std::invoke(std::forward<OneStepFunction>(func),
-                       std::forward<OneStepArgs>(args)...);
-  }
-};
-
 // The simulation function
 template <class OneStepFunction, class... OneStepArgs>
 RealType simulation(const RealType t0, const RealType t1, const RealType dt,
                     OneStepFunction one_step, OneStepArgs&&... function_args) {
   // Call the appropriate overload using the OneStepFunctionSelector
+  static_assert(std::invocable<OneStepFunction, const RealType, const RealType,
+                               OneStepArgs...>);
   auto t = t0;
   while (t < t1) {
-    t = OneStepFunctionSelector<
-        OneStepFunction, const RealType&, const RealType&,
-        OneStepArgs...>::call(std::forward<OneStepFunction>(one_step),t,dt,
-                              std::forward<OneStepArgs>(function_args)...);
+    t = one_step(t, dt, std::forward<OneStepArgs>(function_args)...);
   }
   return t;
 }
