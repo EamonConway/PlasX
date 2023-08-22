c532903ea613b67647f74fc4b56b46a003a4a159
Eamon Conway <conway.e@wehi.edu.au>
2023-08-22 14:40:24 +1000

    stddef header missing.

--- include/PlasX/types.hpp
+++ include/PlasX/types.hpp
@@ -1,17 +1,18 @@
 #ifndef PLASX_TYPES_HPP
 #define PLASX_TYPES_HPP
+#include <stddef.h>
 /**
  * @file types.hpp
  * @author Eamon Conway (conway.e@wehi.edu.au)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2023-03-29
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 namespace plasx {
-  using RealType = double;
-  using SizeType = size_t;
-  }  // namespace plasx
+using RealType = double;
+using SizeType = size_t;
+}  // namespace plasx
 #endif
