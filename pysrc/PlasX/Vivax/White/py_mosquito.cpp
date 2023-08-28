dc1c84200a6aa90eb3fb50b12029cf718347ae4b
Eamon Conway <conway.e@wehi.edu.au>
2023-08-28 16:19:49 +1000

    Allow seasonal kappa function and remove unneccesary copy.
    As the output of odepp::ode_forward_euler is an std::pair we can return the result directly using pybind11/stl.h.
    Removed numpy header.
    Included functional header for seasonality.

--- pysrc/PlasX/Vivax/White/py_mosquito.cpp
+++ pysrc/PlasX/Vivax/White/py_mosquito.cpp
@@ -4,7 +4,7 @@
 #include "PlasX/Vivax/White/mosquito.hpp"
 #include "nlohmann/json.hpp"
 #include "odepp/ode_forward_euler.hpp"
-#include "pybind11/numpy.h"
+#include "pybind11/functional.h"
 #include "pybind11/pybind11.h"
 #include "pybind11/stl.h"
 
@@ -29,12 +29,25 @@ auto mosquito_model(const double lambda, const double kappa, const double dt,
                     const pybind11::dict& parameters) {
   const auto params = pvibm::MosquitoParameters(Params2Json(parameters));
   std::array<RealType, 6> y0 = {1.0};
-  auto [tout, yout] =
-      odepp::ode_forward_euler(mosquito, t0, t1, dt, y0, lambda, kappa, params);
-  return std::make_pair(tout, yout);
+  return odepp::ode_forward_euler(mosquito, t0, t1, dt, y0, lambda, kappa,
+                                  params);
+}
+
+auto seasonal_mosquito_model(
+    const double lambda,
+    const std::function<plasx::RealType(plasx::RealType)>& kappa,
+    const double dt, const double t0, const double t1,
+    const pybind11::dict& parameters) {
+  const auto params = pvibm::MosquitoParameters(Params2Json(parameters));
+  std::array<RealType, 6> y0 = {1.0};
+  return odepp::ode_forward_euler(mosquito, t0, t1, dt, y0, lambda, kappa,
+                                  params);
 }
 
 void add_mosquito_module(pybind11::module_& module) {
   module.def("mosquito_model", &mosquito_model,
              "Run the mosquito model that is used within PVIBM.");
+  module.def("seasonal_mosquito_model", &seasonal_mosquito_model,
+             "Run the mosquito model that is used within PVIBM with a time "
+             "varying kappa function.");
 };
