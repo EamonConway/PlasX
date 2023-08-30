525af3c38bd52769f98160bcf8af329040638d17
Eamon Conway <conway.e@wehi.edu.au>
2023-08-30 12:39:38 +1000

    Root file for pyAPI.

--- pysrc/PlasX/pyPlasX.cpp
+++ pysrc/PlasX/pyPlasX.cpp
@@ -5,14 +5,20 @@
 #include "PlasX/types.hpp"
 #include "nlohmann/json.hpp"
 void add_mosquito_module(pybind11::module_& m);
+void add_equilibrium_module(pybind11::module_& m);
 
 PYBIND11_MODULE(pyPlasX, px) {
   px.doc() =
       "pyPlasX is a python wrapper for the PlasX C++ library. Full "
       "documentation of the C++ library can be found at XX";
-
-  auto px_vivax = px.def_submodule("vivax", "This is the vivax module.");
-  auto px_vivax_white =
-      px_vivax.def_submodule("white", "This is the white module.");
-  add_mosquito_module(px_vivax_white);
+  auto pvibm =
+      px.def_submodule("pvibm",
+                       R"mydelimiter(A Plasmodium Vivax Individual Based Model.
+      
+      Contained within this submodule is a python implementation of the
+      Plasmodium Vivax Individual based model (PVIBM) of White et al (2014).
+      The states that an individual can occupy are also exposed through the
+      status class.)mydelimiter");
+  add_mosquito_module(pvibm);
+  add_equilibrium_module(pvibm);
 }
