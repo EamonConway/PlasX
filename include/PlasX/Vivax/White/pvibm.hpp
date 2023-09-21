c9bdef4f0831e983a28a6f5044e1d597158bbf9e
Eamon Conway <conway.e@wehi.edu.au>
2023-09-21 13:16:45 +1000

    Define equilibrium method of pvibm

---
+++ include/PlasX/Vivax/White/pvibm.hpp
@@ -0,0 +1,13 @@
+#ifndef PLASX_VIVAX_WHITE_PVIBM_HPP
+#define PLASX_VIVAX_WHITE_PVIBM_HPP
+#include "PlasX/Vivax/White/one_step.hpp"
+#include "PlasX/model_simulation.hpp"
+namespace plasx {
+namespace pvibm {
+inline constexpr model_simulation_fn equilibrium(
+    vivax::white::one_step_fn{}, [](RealType t, RealType dt, RealType eir) {
+      return std::make_pair(0, eir);
+    });
+}
+}  // namespace plasx
+#endif
