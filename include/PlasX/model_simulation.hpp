e37e697ccb2f52853ffcbc37e3e0344250f3b763
Eamon Conway <conway.e@wehi.edu.au>
2023-09-21 15:45:05 +1000

    Make code more readable.
    We now use tuple_cat instead of a lambda within std::apply.

--- include/PlasX/model_simulation.hpp
+++ include/PlasX/model_simulation.hpp
@@ -1,10 +1,12 @@
 #ifndef PLASX_MODEL_SIMULATION_FN_HPP
 #define PLASX_MODEL_SIMULATION_FN_HPP
 #include <tuple>
+#include <vector>
 
 #include "PlasX/details/function_args.hpp"
 #include "PlasX/types.hpp"
 namespace plasx {
+
 template <typename HumanModelType, typename MosquitoModelType>
 struct model_simulation_fn {
   constexpr model_simulation_fn(HumanModelType&& human,
@@ -15,31 +17,42 @@ struct model_simulation_fn {
   template <typename Eir, typename... ModelArgs>
   auto operator()(const RealType t0, const RealType t1, const RealType dt,
                   Eir&& initial_eir, ModelArgs&&... model_args) {
-    // Split the arguments into the human and mosquito model.
+    // Initial condition does not have to satisfy the model equations.
+    auto t = t0;
+    auto eir = initial_eir;
+
+    // Split model_args into the two seperate tuples of arguments.
     constexpr auto num_human_args =
         FunctionArgumentCounter<HumanModelType>::value - 3;
     auto [human_args, mosquito_args] =
         splitHumanMosquitoArguments<num_human_args>{}(
             std::forward<ModelArgs>(model_args)...);
 
-    // Initial condition does not have to satisfy the model equations.
-    auto t = t0;
-    // In a perfect world I would use structure bindings for the return type of
-    // human_model_fn and mosquito_model_fn. However, structured bindings cannot
-    // be captured by reference in lambda functions with Clang. This makes the
-    // code less readable in my opinion, but is worth ensuring that there are no
-    // compile errors on different machines.
-    auto eir = initial_eir;
-    while (t < t1) {
+    using HumanModelReturnType = decltype(std::apply(
+        human_model_fn, std::tuple_cat(std::tuple(t, dt, eir), human_args)));
+
+    using MosquitoModelReturnType = decltype(std::apply(
+        mosquito_model_fn,
+        std::tuple_cat(
+            std::tuple(t, dt, std::declval<HumanModelReturnType>().second),
+            mosquito_args)));
+
+    using OutputTypes =
+        std::tuple<RealType, HumanModelReturnType, MosquitoModelReturnType>;
+
+    // Output declaration
+    std::vector<OutputTypes> output;
+
+    while (t <= t1) {
       // Calculate the mosquito to human interaction.
-      auto human_output = std::apply(
+      HumanModelReturnType human_output = std::apply(
           [&](auto&&... args) {
             return human_model_fn(t, dt, eir,
                                   std::forward<decltype(args)>(args)...);
           },
           human_args);
       // Calculate the human mosquito interaction.
-      auto mosquito_output = std::apply(
+      MosquitoModelReturnType mosquito_output = std::apply(
           [&](auto&&... args) {
             return mosquito_model_fn(t, dt, human_output.second,
                                      std::forward<decltype(args)>(args)...);
@@ -47,10 +60,11 @@ struct model_simulation_fn {
           mosquito_args);
 
       eir = mosquito_output.second;
-      [[maybe_unused]] auto combined_output =
-          std::make_pair(human_output, mosquito_output);
+
+      output.emplace_back(std::make_tuple(t, human_output, mosquito_output));
       t += dt;
     }
+    return output;
   };
 
  private:
