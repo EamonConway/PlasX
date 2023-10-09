5dbe24fc0442983c7c0e46f62c2d124ddf09a35b
Eamon Conway <conway.e@wehi.edu.au>
2023-10-09 17:05:34 +1100

    Include getFOI function in multispecies model.
    In reality the state could be arbitrary in the mosquito ode.
    I included getFoi to be called for each species.
    This should integrate nicely.

---
+++ include/PlasX/Vivax/White/multispecies_mosquito_model.hpp
@@ -0,0 +1,59 @@
+#ifndef PLASX_VIVAX_WHITE_MULTISPECIES_MOSQUITO_MODEL_HPP
+#define PLASX_VIVAX_WHITE_MULTISPECIES_MOSQUITO_MODEL_HPP
+#include <concepts>
+#include <map>
+#include <numeric>
+#include <type_traits>
+
+#include "PlasX/types.hpp"
+#include "odepp/integrators/forward_euler.hpp"
+namespace plasx {
+namespace vivax {
+namespace white {
+// Ensure that Mosquito Odes satisfy the same criteria.
+template <typename Fn, typename ModelState, typename ModelParameters>
+concept FoiConcept =
+    std::is_invocable_r_v<RealType, Fn, ModelState, ModelParameters>;
+
+template <typename Model, typename ModelState, typename ModelParameters>
+concept MosquitoOdeConcept =
+    std::is_invocable_r_v<ModelState, Model, RealType, ModelState, RealType,
+                          ModelParameters>;
+
+struct MultiSpeciesMosquitoOdeFn {
+  template <typename ModelState, typename ModelParameters,
+            MosquitoOdeConcept<ModelState, ModelParameters> Model,
+            FoiConcept<ModelState, ModelParameters> FoiFn>
+  auto operator()(RealType t, RealType dt, FoiFn&& getFoi, Model&& model,
+                  RealType lambda,
+                  const std::unordered_map<MosquitoSpecies, ModelState>& state,
+                  const std::unordered_map<MosquitoSpecies, ModelParameters>&
+                      params) const {
+    auto output_state = state;
+
+    const auto AccumulateFoi = [&getFoi, &params](const auto lhs,
+                                                  const auto& rhs) {
+      const auto& [species, species_state] = rhs;
+      return lhs + getFoi(species_state, params.at(species));
+    };
+
+    const auto UpdateMosquitoes = [&model, &dt, &t, &lambda,
+                                   &params](const auto& mosquito) {
+      const auto& [species, species_state] = mosquito;
+      return odepp::integrator::forward_euler(model, dt, t, species_state,
+                                              lambda, params.at(species));
+    };
+
+    auto foi = std::transform_reduce(std::begin(state), std::end(state),
+                                     std::begin(output_state), 0.0,
+                                     UpdateMosquitoes, AccumulateFoi);
+
+    return std::pair(output_state, foi);
+  }
+};
+
+inline constexpr MultiSpeciesMosquitoOdeFn mosquito_ode_model{};
+}  // namespace white
+}  // namespace vivax
+}  // namespace plasx
+#endif  // !PLASX_VIVAX_WHITE_MULTISPECIES_MOSQUITO_MODEL
