2bc305e68b9fd3e97130200eccd74c52c195af8e
Eamon Conway <conway.e@wehi.edu.au>
2023-11-02 18:55:16 +1100

    Mosquito python

--- mosquito_run.py
+++ mosquito_run.py
@@ -3,7 +3,7 @@ from pybin.pyPlasX.mosquito import mosquito_model
 import matplotlib.pyplot as plt
 import numpy as np
 
-parameters = Parameters(death_rate=0.0, zeta=1.0, phi=1.0, gamma=1.0/12.0)
+parameters = Parameters(death_rate=1.0/20.0, zeta=1.0, phi=1.0, gamma=1.0/12.0)
 
 t, y = mosquito_model(0.01, 0.125, 0.0, 500.0, [0.99, 0.01, 0.0],  parameters)
 
