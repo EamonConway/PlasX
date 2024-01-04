69b77e9fd11aa31a85900bbad77e21aede55ef5f
Eamon Conway <conway.e@wehi.edu.au>
2024-01-04 15:48:48 +1100

    Pre view implementation

--- include/PlasX/Mosquito/multispecies_mosquito_model.hpp
+++ include/PlasX/Mosquito/multispecies_mosquito_model.hpp
@@ -8,7 +8,7 @@
 #include "odepp/integrators/forward_euler.hpp"
 namespace plasx {
 namespace mosquito {
-// Ensure that Mosquito Odes satisfy the same criteria.
+
 template <typename Fn, typename ModelState, typename ModelParameters>
 concept FoiConcept =
     std::is_invocable_r_v<RealType, Fn, ModelState, ModelParameters>;
@@ -22,28 +22,32 @@ struct MultiSpeciesMosquitoOdeFn {
   template <typename ModelState, typename ModelParameters,
             MosquitoOdeConcept<ModelState, ModelParameters> Model,
             FoiConcept<ModelState, ModelParameters> FoiFn>
-  [[nodiscard("Ignored return type of MultiSpeciesMosquitoOdeFn")]] auto
-  operator()(RealType& t, RealType dt, RealType lambda, FoiFn&& getFoi,
-             Model&& model,
-             const std::unordered_map<MosquitoSpecies, ModelState>& state,
-             const std::unordered_map<MosquitoSpecies, ModelParameters>& params)
+  [[nodiscard("MultiSpeciesMosquitoOdeFn")]] auto operator()(
+      RealType& t, RealType dt, RealType lambda, FoiFn&& getFoi, Model&& model,
+      const std::unordered_map<MosquitoSpecies, ModelState>& state,
+      const std::unordered_map<MosquitoSpecies, ModelParameters>& params)
       const {
-    auto output_state = state;
-    auto UpdateMosquitoes = [&model, &dt, &t, &lambda, &params,
-                             &getFoi](auto& mosquito) {
-      auto& [species, species_state] = mosquito;
+    // Run the mosquito model over all mosquito species in the unordered_map
+    auto UpdateMosquitoes = [&](const auto& mosquito) {
+      const auto& [species, species_state] = mosquito;
       auto local_t = t;
-      species_state = odepp::integrator::forward_euler(
+      auto updated_species_state = odepp::integrator::forward_euler(
           model, dt, local_t, species_state, lambda, params.at(species));
-      return getFoi(species_state, params.at(species));
+      return getFoi(updated_species_state, params.at(species));
     };
 
-    auto foi =
-        std::transform_reduce(std::begin(output_state), std::end(output_state),
-                              0.0, std::plus(), UpdateMosquitoes);
+    auto UpdateStateAppendTotalFOI = [&](const auto& state) {
+      auto return_value = std::pair(state, 0.0);
+      auto& [output_state, foi] = return_value;
+      foi = std::transform_reduce(std::begin(output_state),
+                                  std::end(output_state), 0.0, std::plus(),
+                                  UpdateMosquitoes);
+      return return_value;
+    };
 
+    auto output = UpdateStateAppendTotalFOI(state);
     t += dt;
-    return std::pair(output_state, foi);
+    return output;
   }
 };
 
