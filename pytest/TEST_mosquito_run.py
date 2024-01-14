e79a9797b19a435d734b04a08dd73d82c0794354
Eamon Conway <conway.e@wehi.edu.au>
2024-01-15 10:35:44 +1100

    Moved mosquito run to a test

--- pytest/TEST_mosquito_run.py
+++ pytest/TEST_mosquito_run.py
@@ -5,7 +5,7 @@ import numpy as np
 
 parameters = Parameters(death_rate=1.0/20.0, zeta=1.0, phi=1.0, gamma=1.0/12.0)
 
-t, y = mosquito_model(0.01, 0.125, 0.0, 500.0, [0.99, 0.01, 0.0],  parameters)
+t, y = mosquito_model(0.201, 0.125, 0.0, 500.0, [0.99, 0.01, 0.0],  parameters)
 
 y_matrix = np.array(y)
 plt.plot(t, y_matrix)
