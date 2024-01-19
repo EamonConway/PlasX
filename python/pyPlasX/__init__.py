b11bb2b82a402fe16da0664699cf853d03859c8d
Eamon Conway <conway.e@wehi.edu.au>
2024-01-19 16:22:03 +1100

    __init__.py and some docstrings

---
+++ python/pyPlasX/__init__.py
@@ -0,0 +1,9 @@
+"""pyPlasX is a python wrapper for the PlasX C++ library.
+
+Full documentation of the C++ library can be found at XX
+
+Typical usage example:
+
+  foo = ClassFoo()
+  bar = foo.FunctionBar()
+"""
